ea45ddd41501823b69bc3eeb95d64b1ebd5f3bf0
daehyeong2 <baconbacon1231@gmail.com>
2024-04-09 20:28:36 +0900

    포트 변경

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import "./db.js";
 import app from "./server.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 function handleListening() {
   console.log(`✅ Server listenting on PORT ${PORT} 🚀`);
